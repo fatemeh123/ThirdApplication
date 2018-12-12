bfdcb181db1897e58e497e2f3d7d37ed68d037bc
Fatemeh <alireza.abdi@Shambhala.local>
2018-12-12 22:50:55 +0100

    webSite included

---
+++ app/src/main/java/com/example/hero/thirdapplication/ThirdActivity.java
@@ -0,0 +1,19 @@
+package com.example.hero.thirdapplication;
+
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        String site =PreferenceManager.getDefaultSharedPreferences(ThirdActivity.this).getString("site","ther is no site to be shown");
+        WebView webView=findViewById(R.id.webView);
+        webView.loadUrl(site);
+    }
+}
